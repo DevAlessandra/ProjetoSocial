a8720354c07a9e9b7ab1f418ca3b0e9570059a7d
DevAlessandra <alecorretorainova@gmail.com>
2026-02-04 11:33:15 -0300

    foote rcorrecao

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from "./footer.module.scss"
 
-export default function Footer() {
+export default function footer() {
   return (
     <div className={s.footer}>
         <h2>© 2025 Vai na Web & Empower. Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos.</h2>
