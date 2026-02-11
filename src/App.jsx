ff25ee92368dc4174196ff40dcfec54499e49054
DevAlessandra <alecorretorainova@gmail.com>
2026-02-11 02:11:03 -0300

    alterado a pasta

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import EventosEP from "./pages/eventosEP/eventosEP"
 import Usuario from "./pages/usuario/Usuario"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
-import Cadastro from "./components/cadastro/Cadastro"
+import Cadastro from "./pages/cadastro/Cadastro"
 
 
 const App = () => {
