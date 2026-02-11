6fcb1f3e023c90c55388826caefdc7b38d1a68a3
DevAlessandra <alecorretorainova@gmail.com>
2026-02-11 01:40:14 -0300

    import cadastro corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import EventosEP from "./pages/eventosEP/eventosEP"
 import Usuario from "./pages/usuario/Usuario"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
-import CadastrarEmpresa from "./components/cadastrarEmpresa/cadastrarEmpresa"
+import CadastrarEmpresa from "./components/cadastrarEmpresa/CadastrarEmpresa"
 
 
 const App = () => {
