d4bddb64eba9b8944e8ed891071ce139c289eed1
DevAlessandra <alecorretorainova@gmail.com>
2026-02-11 01:35:57 -0300

    corrigindo importação

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import EventosEP from "./pages/eventosEP/eventosEP"
 import Usuario from "./pages/usuario/Usuario"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
-import CadastrarEmpresa from "./components/CadastrarEmpresa/cadastrarEmpresa"
+import CadastrarEmpresa from "./components/cadastrarEmpresa/cadastrarEmpresa"
 
 
 const App = () => {
